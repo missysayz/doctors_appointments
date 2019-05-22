e57df697e000ec7b4433f7b44dd1fdb2ad3b1095
Melissa G <jstmac@gmail.com>
2019-05-22 09:52:46 -0600

    last status

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,5 +1,5 @@
 class DoctorsController < ApplicationController
-  before_action :set_doctor, only: [:show, :destroy]
+  before_action :set_doctor, only: [:show, :edit, :destroy]
   
   
   def index
