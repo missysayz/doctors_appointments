facaa0be252b7f6d0e78143ba05c9755a1f84d06
Melissa G <jstmac@gmail.com>
2019-05-22 19:23:43 -0600

    Appointments connected

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,33 +1,37 @@
 class AppointmentsController < ApplicationController
   before_action :set_doctor
+  before_action :set_appointment, only: [:destroy]
   
   def index
-    
+    @appointments = @doctor.appointments.all
   end
 
   def new
-    @users = User.all - @doctor.users
+    @users = User.all
     @appointment = @doctor.appointments.new
   end
 
   def create
     @appointment = @doctor.appointments.new(appointment_params)
     if @appointment.save
-      redirect_to doctor_appointments_path(@doctor)
+      redirect_to @doctor
     else
       render :new
     end
   end
 
   def destroy
-    @appointment = @doctor_appointments.find(params[:id])
     @appointment.destroy
-    redrect_to doctor_appointments_path(@doctor)
+    redirect_to @doctor
   end
 
   private
   def set_doctor
     @doctor = Doctor.find(params[:doctor_id])
+  end
+
+  def set_appointment
+    @appointment = Appointment.find(params[:id])
   end
 
   def appointment_params
